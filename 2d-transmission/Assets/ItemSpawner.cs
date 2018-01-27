ebed011c752bf7afacb0fdcbd1503625192bff0b
hoyinchu <chu.ho@husky.neu.edu>
2018-01-27 13:11:30 -0500

    Added HealthUp, AttackUp, and ItemSpawner

---
+++ 2d-transmission/Assets/ItemSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour {
+
+	public GameObject ItemToSpawn;
+	public int initialSpawnDelay = 0;
+	public int spawnRate = 5;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("SpawnItem",initialSpawnDelay,spawnRate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void SpawnItem() {
+		Instantiate (ItemToSpawn);
+	}
+
+}
