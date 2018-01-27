00021643dbd8fd1c95a22d3c74d7a9f9d1c3856a
TommyOhara <33668237+TommyOhara@users.noreply.github.com>
2018-01-27 14:21:09 -0500

    Level Prototype

---
+++ 2d-transmission/Assets/Scripts/BossController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour {
+
+    int attack;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("MyEvent");
+    }
+
+    private IEnumerator MyEvent()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f); // wait half a second
+            attack = Random.Range(0, 2);
+            //if (attack == 0) && ()
+            {
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
