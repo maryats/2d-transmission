a4276413bb0cbd2b672cfa2d426b845e7c85e004
Marie Yatsyk <maryats@users.noreply.github.com>
2018-01-26 23:22:57 -0500

    PlayerHealth WIP

--- 2d-transmission/Assets/Scripts/PlayerHealth.cs
+++ 2d-transmission/Assets/Scripts/PlayerHealth.cs
@@ -1,16 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
+
+	public int startingHealth = 100;
+	public int currentHealth;
+	public int maxHealth = 100;
+	public Slider healthSlider;
+
 
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+	public void Heal (int amount) {
+
+		if ((currentHealth + amount) <= maxHealth) {
+			currentHealth += amount;
+		}
+	}
+
+	public void TakeDamage (int amount) {
+
+		currentHealth -= amount;
+
+		healthSlider.value = currentHealth;
+
+		if (currentHealth <= 0) {
+			Die ();
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void Die () {
+		playerMovement.enabled = false;
 	}
 }
