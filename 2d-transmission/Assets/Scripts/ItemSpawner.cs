2cd49194b82134c6e8b249369af6fbc30911bd95
RogerChen98 <chen.rog@husky.neu.edu>
2018-01-27 14:59:19 -0500

    Item spawner Version 1.0

--- 2d-transmission/Assets/Scripts/ItemSpawner.cs
+++ 2d-transmission/Assets/Scripts/ItemSpawner.cs
@@ -5,21 +5,46 @@ using UnityEngine;
 public class ItemSpawner : MonoBehaviour {
 
 	public GameObject ItemToSpawn;
-	public int initialSpawnDelay = 0;
-	public int spawnRate = 5;
+	public int spawnRate;
+
+	private Vector2 location;
+	private int timeTilNextSpawn;
 
 	// Use this for initialization
 	void Start () {
-		InvokeRepeating ("SpawnItem",initialSpawnDelay,spawnRate);
+		// Spawn an initial one
+		SpawnItem ();
+
+		// Get the location of this object instead of the duplicated object
+		float x = transform.position.x;
+		float y = transform.position.y;
+		location = new Vector2 (x, y);
+
+		// Sets the time until the next spawn equal to the spawn rate
+		timeTilNextSpawn = spawnRate;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		timeTilNextSpawn--;
 
+		// Spawn a new item if there has been a delay equal to or greater than spawnRate
+		if (timeTilNextSpawn <= 0) {
+			timeTilNextSpawn = spawnRate;
+			SpawnItem ();
+		}
 	}
 
+	// Reset the timer if the item is still active
+	void OnCollisionEnter(Collision collision) {
+		if (collision.transform.CompareTag(ItemToSpawn.transform.tag) {
+			timeTilNextSpawn = spawnRate;
+		}
+	}
+
+	// This actually spawns the item
 	void SpawnItem() {
-		Instantiate (ItemToSpawn);
+		Instantiate (ItemToSpawn, location);
 	}
 
 }
