9a5c6c83900266b3d8990fab6fc9a34a268ff4b3
RogerChen98 <chen.rog@husky.neu.edu>
2018-01-27 19:36:03 -0500

    Sick items

---
+++ 2d-transmission/Assets/Scripts/AttackUpItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackUpItem : MonoBehaviour {
+	public int baseDamage;
+	public int attackIncreaseAmount;
+
+	private static int damageBuff;
+	private int damage;
+
+	// Use this for initialization
+	void Start () {
+		damageBuff = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Always call on pickup so that the attackupitem damage can be modified
+	void pickup() {
+		damage = damage + baseDamage;
+		damageBuff += attackIncreaseAmount;
+		Destroy (this.gameObject);
+	}
+
+	// For the player to get the damage
+	public int getDamage() {
+		return damage;
+	}
+}
