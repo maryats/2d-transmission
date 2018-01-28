c90bece076ec471094c438d79271fa8e4171b240
TommyOhara <33668237+TommyOhara@users.noreply.github.com>
2018-01-28 09:32:03 -0500

    Health Chip Spawn

--- 2d-transmission/Assets/Scripts/HealthUpItem.cs
+++ 2d-transmission/Assets/Scripts/HealthUpItem.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class HealthUpItem : MonoBehaviour {
 
-	public int destroySeconds;
+	//public int destroySeconds;
 	public int recoverAmount;
     GameObject gatherer;
     public PlayerHealth gathererHealth;
+    public float spawnTime;
 
     // Use this for initialization
     void Start () {
         gatherer = GameObject.Find("Gatherer");
         gathererHealth = gatherer.GetComponent<PlayerHealth>();
-        destroySeconds = 60;
+        //destroySeconds = 60;
         recoverAmount = 20;
-        Destroy (this.gameObject, destroySeconds);
+       // Destroy (this.gameObject, destroySeconds);
 	}
 	
 	// Update is called once per frame
@@ -30,8 +31,20 @@ public class HealthUpItem : MonoBehaviour {
         {
 
             gathererHealth.Heal(recoverAmount);
-            Destroy(this.gameObject);
+            gameObject.SetActive(false);
+
+            if (gameObject.activeSelf == false)
+            {
+                Debug.Log("Item Destroyed");
+                Invoke("SpawnItem", spawnTime);
+            }
 
         }
+    }
+
+    void SpawnItem()
+    {
+        Debug.Log("Item Spawned");
+        gameObject.SetActive(true);
     }
 }
